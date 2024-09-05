fcf9140ceee1b34a6af0833a4a0fd4c2c5b7abb6
Dave Sharma <dave.sharma002@gmail.com>
2024-09-05 07:45:53 +0530

    New Files

--- MCA Trimester 1/Web Stack/Web4_5/transformation.py
+++ MCA Trimester 1/Web Stack/Web4_5/transformation.py
@@ -1,11 +1,9 @@
 import lxml.etree as ET
 
-# Load XML and XSD
-xml_doc = ET.parse("formula_one_circuits.xml") 
-xsd_doc = ET.parse("formula_one_circuits.xsd")
+xml_doc = ET.parse("E:/New folder/Christ-University/MCA Trimester 1/Web Stack/Web4_5/formula_one_circuits.xml")
+xsd_doc = ET.parse("E:/New folder/Christ-University/MCA Trimester 1/Web Stack/Web4_5/formula_one_circuits.xsd")
 xmlschema = ET.XMLSchema(xsd_doc)
 
-# Validate
 is_valid = xmlschema.validate(xml_doc)
 if not is_valid:
     print("XML data is invalid:")
@@ -14,10 +12,9 @@ if not is_valid:
 else:
     print("XML data is valid according to the schema.")
 
-    # Proceed with transformation if valid
-    xslt = ET.parse("transform.xsl")
+    xslt = ET.parse("E:/New folder/Christ-University/MCA Trimester 1/Web Stack/Web4_5/transform.xsl")
     transform = ET.XSLT(xslt)
     newdom = transform(xml_doc)
-    with open('output.html', 'wb') as f:
+    with open('E:/New folder/Christ-University/MCA Trimester 1/Web Stack/Web4_5/output.html', 'wb') as f:
         f.write(ET.tostring(newdom, pretty_print=True))
     print("HTML output generated successfully!")
